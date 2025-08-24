fdba1be45b5b6885c358856deba8ca873892e6d5
Fatemah Alazran <152088929+FatemahAlazran@users.noreply.github.com>
2025-08-24 19:49:52 +0300

    done with add and remove functions
    done with add and remove functions

--- script.js
+++ script.js
@@ -1,14 +1,33 @@
 // Create a empty array to store the names here 
-
+let names = [];
 
 // create a function that can add names to the array and print it in the list here 
 
+function addName(){
 
+    let inputval = document.getElementById('name').value;
+    names.push(inputval);
+    
+    [inputval].forEach(e => {
+    
+        document.getElementById("namesList").innerHTML += "<li>" + e + "</li>"
+    
+    });
+
+    console.log(names);
+    }
 
 
 // create a function that can remove the last name from the array and print it in the list here 
 
+function removeName(){
 
+    names.pop();
+    let remover= document.getElementById("namesList");
+    
+    remover.removeChild(remover.lastChild);
+    console.log(names);
+    }
 
 
 
